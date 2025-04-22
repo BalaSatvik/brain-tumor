248b5c5ee637078ba56cb6eb23cbca89175e2e10
BalaSatvik <namburibalasatvik@gmail.com>
2025-04-22 22:39:54 +0530

    sc

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,11 +11,8 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import './home.css';
-import { companyInfo } from '../companyInfo';
-import ChatbotIcon from './ChatbotIcon';
-import ChatForm from './ChatForm';
-import ChatMessage from './ChatMessage';
 import { textAlign } from "@mui/system";
+import Chat from "./Chat";
 
 
 const Home = ({details}) => {
@@ -95,75 +92,75 @@ const Home = ({details}) => {
         overflowY :"Scroll",
       };
 
-      const [chatHistory,setChatHistory]=useState([
-          {
-            hideInChat:true,
-            role:"model",
-            text:companyInfo,
-          },
-        ]);
+      // const [chatHistory,setChatHistory]=useState([
+      //     {
+      //       hideInChat:true,
+      //       role:"model",
+      //       text:companyInfo,
+      //     },
+      //   ]);
       
-        //Extra
-        const [ user, setLoginUser] = useState({});
+      //   //Extra
+      //   const [ user, setLoginUser] = useState({});
         
-        var props={
-          user:user,
-          setLoginUser:setLoginUser
-        }
-        const [showChatbot,setShowChatbot]=useState(false);
-        const chatBodyRef=useRef();
+      //   var props={
+      //     user:user,
+      //     setLoginUser:setLoginUser
+      //   }
+      //   const [showChatbot,setShowChatbot]=useState(false);
+      //   const chatBodyRef=useRef();
       
       
-        const generateBotResponse = async (history) => {
-          const updateHistory = (text,isError = false) => {
-            setChatHistory(prev => [...prev.filter(msg => msg.text !== "Thinking....."),{role:"model",text, isError}]);
-          }
-          history = history.map(({role,text}) => ({role,parts: [{text}]}));
-          const requestOptions = {
-            method: "POST",
-            headers: {"Content-Type": "application/json" },
-            body: JSON.stringify({contents: history})
-          }
-          try{
-            const response = await fetch(import.meta.env.VITE_API_URL,requestOptions);
-            const data=await response.json();
-            if (!response.ok) throw new Error(data.error.message || "Something went wrong!");
-            const apiResponseText=data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g,"$1").trim();
-            updateHistory(apiResponseText);
-          } catch(error){
-            updateHistory(error.message,true);
-          }
-        };
+      //   const generateBotResponse = async (history) => {
+      //     const updateHistory = (text,isError = false) => {
+      //       setChatHistory(prev => [...prev.filter(msg => msg.text !== "Thinking....."),{role:"model",text, isError}]);
+      //     }
+      //     history = history.map(({role,text}) => ({role,parts: [{text}]}));
+      //     const requestOptions = {
+      //       method: "POST",
+      //       headers: {"Content-Type": "application/json" },
+      //       body: JSON.stringify({contents: history})
+      //     }
+      //     try{
+      //       const response = await fetch(import.meta.env.VITE_API_URL,requestOptions);
+      //       const data=await response.json();
+      //       if (!response.ok) throw new Error(data.error.message || "Something went wrong!");
+      //       const apiResponseText=data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g,"$1").trim();
+      //       updateHistory(apiResponseText);
+      //     } catch(error){
+      //       updateHistory(error.message,true);
+      //     }
+      //   };
       
-        useEffect(() => {
-          chatBodyRef.current.scrollTo({top: chatBodyRef.current.scrollHeight, behavior: "smooth"});
-        },[chatHistory]);
+      //   useEffect(() => {
+      //     chatBodyRef.current.scrollTo({top: chatBodyRef.current.scrollHeight, behavior: "smooth"});
+      //   },[chatHistory]);
 
     return (
-        <header style={HeaderStyle}>
-
-        <div className="home">
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                        >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} className="heading">
-                        Brain - Tumor
-                    </Typography>
-                    <Button color="inherit" onClick={() => details.setLoginUser({})}>Logout</Button>
-                </Toolbar>
-            </AppBar>
-            <br />
-            <div className="bp">
-            <Grid container spacing={2} justifyContent="center" style={{ paddingLeft: "30px", paddingTop: "10px" }}>
-              <Grid item xs={11}>
+      
+      <div className="home">
+          <header style={HeaderStyle}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} className="heading">
+                Brain - Tumor
+              </Typography>
+              <Button color="inherit" onClick={() => details.setLoginUser({})}>Logout</Button>
+            </Toolbar>
+          </AppBar>
+          <br />
+          <div className="bp">
+            <Grid container spacing={2} columns={12} justifyContent="center" style={{ paddingLeft: "30px", paddingTop: "10px" }}>
+              <Grid span={11}>
                 <Typography variant="h3" sx={{ width: '100%', textAlign: 'center' }}>
                   Welcome {details?.user?.name || "Guest"} !!!
                 </Typography>
@@ -172,88 +169,58 @@ const Home = ({details}) => {
 
             <br />
             <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
             >
-                <div className="homepage">
-                    <input type='file' onChange={onSelectFile} /> <br /><br />
-                    {selectedFile && <img src={preview} style={{ maxWidth: "800px", maxHeight: "600px" }} />}<br /> <br />
-                    <Button variant="contained" color="primary" onClick={detect} >
-                        Detect
-                    </Button><br /><br />
-                </div>
+              <div className="homepage">
+                <input type='file' onChange={onSelectFile} /> <br /><br />
+                {selectedFile && <img src={preview} style={{ maxWidth: "800px", maxHeight: "600px" }} />}<br /> <br />
+                <Button variant="contained" color="primary" onClick={detect}>
+                  Detect
+                </Button><br /><br />
+              </div>
             </Box>
 
 
             {processing1 && processing2 ? (<h1>processing.......</h1>) :
-                (<div>
-                    {detected && <h1 style={{ paddingLeft: "50px" }}>Detected Image:</h1>}
-                    <Grid container spacing={2} style={{ paddingLeft: "200px", paddingTop: "10px" }}>
-                        <Grid xs={8}>
-                            <div>
-                                {detected && <img src={detected} style={{ maxWidth: "800px", maxHeight: "600px" }} />}
-                            </div>
-                        </Grid>
-                        <Grid xs={4}>
-                            <div>
-                                {detected && <h2>Detected human:</h2>}
-                                {detected &&
-                                    <ul>{toolslist.length > 0 && toolslist.map((item) => <li key={item}> <h3>{item}</h3> </li>)}</ul>
-                                }
-                            </div>
-                        </Grid>
-                    </Grid>
-                    {semantic && <h1 style={{ paddingLeft: "50px" }}>Segmented Image:</h1>}<br />
-                    <Grid container spacing={2} style={{ paddingLeft: "200px", paddingTop: "10px" }}>
-                        <Grid xs={8}>
-                            <div>
-                                {semantic && <img src={semantic} style={{ maxWidth: "800px", maxHeight: "600px" }} />}
-                            </div>
-                        </Grid>
-                        <Grid xs={4}>
-                            <div>
-                                {semantic && <Legend />}
-                            </div>
-                        </Grid>
-                    </Grid>
-                    
-                </div>)}
-                </div>
-                <button onClick={() => setShowChatbot(prev => !prev)} id="chatbot-toggler">
-        <span className="material-symbols-rounded">mode_comment</span>
-        <span className="material-symbols-rounded">close</span>
-      </button>
-      <div className={`container ${showChatbot?"show-chatbot":""}`}>
+              (<div>
+                {detected && <h1 style={{ paddingLeft: "50px" }}>Detected Image:</h1>}
+                <Grid container spacing={2} style={{ paddingLeft: "200px", paddingTop: "10px" }}>
+                  <Grid span={8}>
+                    <div>
+                      {detected && <img src={detected} style={{ maxWidth: "800px", maxHeight: "600px" }} />}
+                    </div>
+                  </Grid>
+                  <Grid span={4}>
+                    <div>
+                      {detected && <h2>Detected human:</h2>}
+                      {detected &&
+                        <ul>{toolslist.length > 0 && toolslist.map((item) => <li key={item}> <h3>{item}</h3> </li>)}</ul>}
+                    </div>
+                  </Grid>
+                </Grid>
+                {semantic && <h1 style={{ paddingLeft: "50px" }}>Segmented Image:</h1>}<br />
+                <Grid container spacing={2} style={{ paddingLeft: "200px", paddingTop: "10px" }}>
+                  <Grid span={8}>
+                    <div>
+                      {semantic && <img src={semantic} style={{ maxWidth: "800px", maxHeight: "600px" }} />}
+                    </div>
+                  </Grid>
+                  <Grid span={4}>
+                    <div>
+                      {semantic && <Legend />}
+                    </div>
+                  </Grid>
+                </Grid>
 
-        <div className="chatbot-popup">
-          <div className="chat-header">
-            <div className="header-info">
-              <ChatbotIcon />
-              <h2 className="logo-text">Chatbot</h2>
-            </div>
-            <button onClick={() => setShowChatbot(prev => !prev)} className="material-symbols-rounded">keyboard_arrow_down</button>
+              </div>)}
           </div>
-          <div ref={chatBodyRef} className="chat-body">
-            <div className="message bot-message">
-              <ChatbotIcon />
-              <p className="message-text">
-                Hey There <br /> How can I help you today?
-              </p>
-            </div>
+      </header>
 
-            {chatHistory.map((chat,index) =>(
-              <ChatMessage key={index} chat={chat} />
-            ))}
-          </div>
-          <div className="chat-footer">
-            <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} generateBotResponse={generateBotResponse}/>
-          </div>
-        </div>
-          </div>
+      <Chat />
       </div>
-                                </header>
-    )
+    );
 }
 
 export default Home;
